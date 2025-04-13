ff9d4460082e225d4a694006822ffbd29dc0ebce
Alex Uskov <FrozenAlex@users.noreply.github.com>
2025-04-13 11:45:42 +0400

    Update defaults

--- include/GraphicsTweaksConfig.hpp
+++ include/GraphicsTweaksConfig.hpp
@@ -6,7 +6,7 @@ DECLARE_CONFIG(GraphicsTweaksConfig) {
     CONFIG_VALUE(DynamicResolution, bool, "Dynamic Resolution", false);
     CONFIG_VALUE(MenuResolution, float, "Menu Resolution", 1);
     CONFIG_VALUE(GameResolution, float, "Game Resolution", 1);
-    CONFIG_VALUE(AntiAliasing, int, "Anti Aliasing", 2);
+    CONFIG_VALUE(AntiAliasing, int, "Anti Aliasing", 0); // 0 = Off, 1 = 2x, 2 = 4x
     CONFIG_VALUE(MenuRefreshRate, float, "Menu Refresh Rate", -1); // We don't know the Refresh Rate, at this time.
     CONFIG_VALUE(GameRefreshRate, float, "Game Refresh Rate", -1); // We don't know the Refresh Rate, at this time.
     CONFIG_VALUE(Bloom, bool, "Bloom", false);
@@ -15,17 +15,15 @@ DECLARE_CONFIG(GraphicsTweaksConfig) {
     CONFIG_VALUE(Mirror, int, "Mirror", 1); // 0 = Off, 1 = Low, 2 = Medium, 3 = High
     CONFIG_VALUE(SmokeQuality, int, "SmokeQuality", 1); // 0 = Off, 1 = Low, 3 = High
     CONFIG_VALUE(WallQuality, int, "WallQuality", 1); // 0 = Transparent, 1 = Textured, 2 = Distorted
-    CONFIG_VALUE(EnhancedLQMirror, bool, "EnhancedLQMirror", true);
     CONFIG_VALUE(FpsCounter, bool, "FpsCounter", true);
     CONFIG_VALUE(FpsCounterAdvanced, bool, "FpsCounterAdvanced", false);
-    CONFIG_VALUE(LightBrightness, float, "LightBrightness", 1);
     CONFIG_VALUE(MenuFoveatedRenderingLevel, int, "MenuFoveatedRenderingLevel", 0);
     CONFIG_VALUE(InGameFoveatedRenderingLevel, int, "InGameFoveatedRenderingLevel", 4);
-    CONFIG_VALUE(ColorSpace, int, "ColorSpace", 4);
+    CONFIG_VALUE(ColorSpace, int, "ColorSpace", 6);
     CONFIG_VALUE(CpuPerfLevel, int, "CpuPerfLevel", 1); // 0 = Low, 1 = SustainedLow, 2 = SustainedHigh
     CONFIG_VALUE(GpuPerfLevel, int, "GpuPerfLevel", 2); // 0 = Low, 1 = SustainedLow, 2 = SustainedHigh
-    CONFIG_VALUE(Burnmarks, bool, "Burnmarks", true);
-    CONFIG_VALUE(MenuShockwaves, bool, "MenuShockwaves", true);
-    CONFIG_VALUE(GameShockwaves, bool, "GameShockwaves", true);
+    CONFIG_VALUE(Burnmarks, bool, "Burnmarks", false);
+    CONFIG_VALUE(MenuShockwaves, bool, "MenuShockwaves", false);
+    CONFIG_VALUE(GameShockwaves, bool, "GameShockwaves", false);
     CONFIG_VALUE(UsedGraphicsPresetBefore, bool, "UsedGraphicsPresetBefore", false);
 };
